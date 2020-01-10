dc42135d2e6ebec982c6882316e089c9e37abefc
Dexshine <b_o_y_s@hotmail.com>
2020-01-10 15:16:06 +0700

    ver1

--- rehearsal_2.cpp
+++ rehearsal_2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    double x,sum=0;
+    while(x!=0){
+        cout << "Enter x: ";
+        cin >> x;
+        if(x>0){
+            sum = sum + x;
+        }
 
+    }
+    cout << "sum = " << sum;
+
+    
+    return 0;
+}
